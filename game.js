4f067fd0b5580fbc169780a13e4cddbf74b9bd68
Anirudh VSP <anirudhvsp@gmail.com>
2024-11-10 10:59:40 +0530

    Updated to state architecture

--- game.js
+++ game.js
@@ -39,6 +39,22 @@ let ballConfigs = [
 
 const brickHeight = 30;
 const brickWidth = 60;
+
+// Add game state object
+const gameState = {
+    paddles: {
+        black: { x: 0, y: 0, width: 150, height: 25 },
+        white: { x: 0, y: 0, width: 150, height: 25 }
+    },
+    balls: [],
+    bricks: [],
+    scores: {
+        black: 0,
+        white: 0
+    },
+    mouseX: 0
+};
+
 function preload() {
     // Create a graphics object to use as the ball
     this.graphics = this.add.graphics();
@@ -69,8 +85,20 @@ function preload() {
 
 
 function create() {
-    // Update paddle creation to use textures
+    // Initialize paddle positions in state
+    this.blackScore = 0; // Initialize black score
+    this.whiteScore = 0; // Initialize white score
+    this.blackScoreText = this.add.text(15, config.height - 55, '0', { fontSize: '32px', fill: '#000000' }); // Bottom left
+    this.whiteScoreText = this.add.text(config.width - 75, 10, ' 0', { fontSize: '32px', fill: '#FFFFFF' }); // Top right
+    this.whiteScoreText.setDepth(1);
+    this.blackScoreText.setDepth(1);
+
+
+
     paddleConfigs.forEach(config => {
+        gameState.paddles[config.type].x = config.x;
+        gameState.paddles[config.type].y = config.y;
+        
         let paddle = this.physics.add.image(config.x, config.y, config.texture)
             .setImmovable(true);
         paddle.body.allowGravity = false;
@@ -79,39 +107,53 @@ function create() {
         paddles.push(paddle);
     });
 
-    // Mouse control for black paddle only
+    // Update mouse handler to modify state instead of paddle directly
     this.input.on('pointermove', function (pointer) {
-        let blackPaddle = paddles.find(p => p.getData('type') === 'black');
-        blackPaddle.x = Phaser.Math.Clamp(pointer.x, blackPaddle.width/2, config.width - blackPaddle.width/2);
+        gameState.mouseX = pointer.x;
     });
 
-    this.blackScore = 0; // Initialize black score
-    this.whiteScore = 0; // Initialize white score
-    this.blackScoreText = this.add.text(15, config.height - 55, '0', { fontSize: '32px', fill: '#000000' }); // Bottom left
-    this.whiteScoreText = this.add.text(config.width - 75, 10, ' 0', { fontSize: '32px', fill: '#FFFFFF' }); // Top right
-    this.whiteScoreText.setDepth(1);
-    this.blackScoreText.setDepth(1);
-
+    // Initialize balls in state
     ballConfigs.forEach(config => {
-        balls.push(createBall(this, config.x, config.y, config.texture, config.type));
+        const ballState = {
+            x: config.x,
+            y: config.y,
+            velocityX: 500,
+            velocityY: config.type === 'white' ? 500 : -500,
+            type: config.type
+        };
+        gameState.balls.push(ballState);
+        balls.push(createBall(this, ballState));
     });
-    // Create bricks
-    bricks = this.physics.add.staticGroup();
+
+    // Initialize bricks in state
     for (let y = brickHeight/2; y < config.height-brickHeight/2; y += brickHeight) {
         for (let x = brickWidth/2; x < config.width-brickWidth/3; x += brickWidth) {
-            if(y<config.height/2){
-                let brick = this.add.rectangle(x, y, brickWidth, brickHeight, 0x000000);
-                brick.setData('type', "black");
-                bricks.add(brick);
-            }
+            var type,color;
+            if(y > config.height/2){
+                type = "white"
+                color = 0xFFFFFF
+            } 
             else{
-                let brick = this.add.rectangle(x, y, brickWidth, brickHeight, 0xFFFFFF);
-                brick.setData('type', "white");
-                bricks.add(brick);
+                type = "black"
+                color = 0x000000
             }
+            const brickState = {
+                x: x,
+                y: y,
+                type: type,
+                color :color
+                
+            };
+            
+            gameState.bricks.push(brickState);
         }
     }
-
+    bricks = this.physics.add.staticGroup();
+    for(i in gameState.bricks){
+        let brick = this.add.rectangle(gameState.bricks[i].x,gameState.bricks[i].y,brickWidth, brickHeight, gameState.bricks[i].color);
+        brick.setData('type', gameState.bricks[i].type);
+        bricks.add(brick);
+    }
     balls.forEach(ball => {
         paddles.forEach(paddle => {
             this.physics.add.collider(ball, paddle, hitPaddle, null, this);
@@ -124,32 +166,40 @@ function create() {
     //     right: Phaser.Input.Keyboard.KeyCodes.D
     // });
 }
-// Function to create a ball
-function createBall(context, x, y, texture, type) {
-    let ball = context.physics.add.image(x, y, texture);
-    ball.setDisplaySize(20, 20); // Set size to look like a circle
+
+// Update createBall to use state
+function createBall(context, ballState) {
+    let ball = context.physics.add.image(ballState.x, ballState.y, 
+        ballState.type === 'black' ? 'ball' : 'ballWhite');
+    ball.setDisplaySize(20, 20);
     ball.setCollideWorldBounds(true);
-    ball.setBounce(1); // Reflective bounce
-    ball.setData('type', type);
+    ball.setBounce(1);
+    ball.setData('type', ballState.type);
     ball.setDepth(1);
-    if (type === 'white') {
-        const randomX = Phaser.Math.Between(-500, 500); // Random X velocity
-        const randomY = Phaser.Math.Between(-500, -300); // Random Y velocity (upward)
-        ball.setVelocity(randomX, randomY);
-    } else {
-        ball.setVelocity(500, -500); // Initial ball velocity for black balls
-    }
+    ball.setVelocity(ballState.velocityX, ballState.velocityY);
     return ball;
 }
+
+// Modify update function to sync game objects with state
 function update() {
-    // Paddle movement
-    // if (cursors.left.isDown) {
-    //     paddle.setVelocityX(-1000);
-    // } else if (cursors.right.isDown) {
-    //     paddle.setVelocityX(1000);
-    // } else {
-    //     paddle.setVelocityX(0);
-    // }
+    // Update black paddle position based on mouse state
+    const blackPaddle = paddles.find(p => p.getData('type') === 'black');
+    const clampedX = Phaser.Math.Clamp(
+        gameState.mouseX, 
+        blackPaddle.width/2, 
+        config.width - blackPaddle.width/2
+    );
+    gameState.paddles.black.x = clampedX;
+    blackPaddle.x = clampedX;
+
+    // Update ball positions in state
+    balls.forEach((ball, index) => {
+        gameState.balls[index].x = ball.x;
+        gameState.balls[index].y = ball.y;
+        gameState.balls[index].velocityX = ball.body.velocity.x;
+        gameState.balls[index].velocityY = ball.body.velocity.y;
+    });
+
 }
 
 function hitPaddle(ball, paddle) {
@@ -157,22 +207,26 @@ function hitPaddle(ball, paddle) {
     ball.setVelocityX(10 * diff); // Change angle based on hit position
 }
 
+// Modify hitBrick to update state
 function hitBrick(ball, brick) {
-    if(ball.getData('type') == brick.getData('type')){
-        if(brick.getData('type') == "black"){
-            brick.setFillStyle(0xFFFFFF);
-            brick.setData("type", "white");
-            this.blackScore++; // Increment black score
-            this.blackScoreText.setText(this.blackScore); // Update score display
+    if(ball.getData('type') == brick.getData('type')) {
+        const newType = brick.getData('type') === 'black' ? 'white' : 'black';
+        
+        // Update brick state
+        const brickState = gameState.bricks.find(b => b.x === brick.x && b.y === brick.y);
+        brickState.type = newType;
+        
+        // Update score in state
+        if(newType === 'white') {
+            gameState.scores.black++;
+            this.blackScoreText.setText(gameState.scores.black);
         } else {
-            brick.setFillStyle(0x000000);
-            brick.setData("type", "black");
-            this.whiteScore++; // Increment white score
-            this.whiteScoreText.setText(this.whiteScore); // Update score displayd
+            gameState.scores.white++;
+            this.whiteScoreText.setText(gameState.scores.white);
         }
-    }
-    else{
-        return;
+        
+        brick.setFillStyle(newType === 'black' ? 0x000000 : 0xFFFFFF);
+        brick.setData('type', newType);
     }
 }
 
